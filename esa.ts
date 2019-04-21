eac676503ed5a4023f2021b901b0c518537c19f9
mallowlabs <mallowlabs@gmail.com>
2019-04-21 21:31:08 +0900

    Added: esa.ts

---
+++ esa.ts
@@ -0,0 +1,50 @@
+const esaTeams = PropertiesService.getScriptProperties().getProperty('ESA_TEAMS').split(',');
+const esaToken = PropertiesService.getScriptProperties().getProperty('ESA_TOKEN');
+const esaUser = PropertiesService.getScriptProperties().getProperty('ESA_USER');
+const webhookUrl = PropertiesService.getScriptProperties().getProperty('SLACK_WEBHOOK_URL');
+
+function mainEsa(): void {
+    const today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+
+    let contents = '';
+    for(let team of esaTeams) {
+        const posts = fetchEsa(team);
+        for(let post of posts) {
+            if (new Date(post.created_at) < today) {
+                break;
+            }
+            contents += `:esa: [${post.name}](${post.url})\n`;
+        }
+    }
+    if (contents === '') {
+        contents = ':esa: 作成した記事はありません。'
+    }
+    postSlack(contents);
+}
+
+function fetchEsa(team: string): any[] {
+    const options : GoogleAppsScript.URL_Fetch.URLFetchRequestOptions =
+    {
+        'method' : 'get',
+        'contentType' : 'application/json',
+        'headers' : { 'Authorization': `Bearer ${esaToken}` }
+    };
+    const response = UrlFetchApp.fetch(`https://api.esa.io/v1/teams/${team}/posts?sort=created&q=user%3A${esaUser}`, options);
+    return JSON.parse(response.getContentText()).posts;
+}
+
+function postSlack(body: string): void {
+    const payload = JSON.stringify({'text' : body});
+
+    const options : GoogleAppsScript.URL_Fetch.URLFetchRequestOptions =
+    {
+        'method' : 'post',
+        'contentType' : 'application/json',
+        'payload' : payload
+    };
+
+    UrlFetchApp.fetch(webhookUrl, options);
+}
